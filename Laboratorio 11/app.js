81559e6e047580887c43f3a3bfc9c6edccc6769d
Pablo Hazael Hurtado Mireles <h.h.m.pablo@gmail.com>
2024-02-27 10:16:22 -0600

    Agregacion de app.use

---
+++ Laboratorio 11/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use('/construir', (request, response, next) => {
+    response.send('Respuesta de la ruta "/construir"'); 
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+    next();
+});
+
+app.listen(3000);
