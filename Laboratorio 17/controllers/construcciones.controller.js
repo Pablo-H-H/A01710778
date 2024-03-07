3ea2cfc3c0cc3e6ef717af11380c959aa5b3298c
Pablo Hazael Hurtado Mireles <h.h.m.pablo@gmail.com>
2024-03-07 09:16:48 -0600

    Creacion de la rama

---
+++ Laboratorio 17/controllers/construcciones.controller.js
@@ -0,0 +1,31 @@
+const Construccion = require('../models/construccion.model')
+
+exports.get_construir = (request, response, next) => {
+    response.render('construir', {username: request.body.username || ''
+    });
+};
+
+exports.post_contruir = (request, response, next) => {
+    console.log(request.body);
+    const construccion = new Construccion(request.body.nombre, request.body.imagen);
+    construccion.save();
+
+    response.setHeader('Set-Cookie', 'ultima-construccion=' + request.body.nombre + '; HTTPOnly');
+    response.redirect('/');
+}
+
+exports.get_root = (request, response, next) => {
+    console.log('Ruta "/"');
+    let ultima_construccion = request.get('Cookie');
+    if(ultima_construccion){
+        ultima_construccion = ultima_construccion.split('=')[1]
+    } else{
+        ultima_construccion = '';
+    }
+    console.log(ultima_construccion);
+    response.render('construcciones', {
+        construcciones: Construccion.fetchAll(),
+        ultima_construcciones: ultima_construccion,
+        username: request.session.username || '',
+    });
+}
