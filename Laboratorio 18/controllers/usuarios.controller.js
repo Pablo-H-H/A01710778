fbc7d9250a6d9e332b2aa3ba840f8c5cbcee0041
Pablo Hazael Hurtado Mireles <h.h.m.pablo@gmail.com>
2024-03-12 09:02:30 -0600

    Encryptacion y comprobacion del login

---
+++ Laboratorio 18/controllers/usuarios.controller.js
@@ -0,0 +1,59 @@
+const Usuario = require('../models/usuario.model');
+const bcrypt = require('bcryptjs');
+
+
+exports.get_login = (request, response, next) => {
+    response.render('login', {
+        username: request.body.username || '',
+        registrar: false,
+    });
+};
+
+exports.post_login = (request, response, next) => {
+    Usuario.fetchOne(request.body.username)
+        .then(([users, fieldData]) => {
+            if(users.length == 1) {
+                //users[0] contiene el objeto de la respuesta de la consulta
+                const user = users[0];
+                bcrypt.compare(request.body.password, user.password)
+                    .then(doMatch => {
+                        if (doMatch) {
+                            request.session.isLoggedIn = true;
+                            request.session.username = user.username;
+                            return request.session.save(err => {
+                                response.redirect('/construcciones');
+                            });
+                        } else {
+                            return response.redirect('/users/login');
+                        }
+                    }).catch(err => {
+                        response.redirect('/users/login');
+                    });
+            } else {
+                response.redirect('/users/login');
+            }
+        })
+        .catch((error) => {console.log(error)});
+};
+
+exports.get_logout = (request, response, next) => {
+    request.session.destroy(() => {
+        response.redirect('/users/login'); //Este código se ejecuta cuando la sesión se elimina.
+    });
+};
+
+exports.get_signup = (request, response, next) => {
+    response.render('login', {
+        username: request.body.username || '',
+        registrar: true,
+    });
+}
+
+exports.post_signup = (request, response, next) => {
+    const nuevo_usuario = new Usuario(request.body.username, request.body.password);
+    nuevo_usuario.save()
+        .then(([rows, fieldData])=>{
+            response.redirect('/users/login');
+        })
+        .catch((error)=>{console.log(error);});
+};
