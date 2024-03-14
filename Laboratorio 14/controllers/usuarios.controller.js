267ec61d959ba5f77accbcdf2bf63b2897f1ac23
Pablo Hazael Hurtado Mireles <h.h.m.pablo@gmail.com>
2024-03-14 14:11:09 -0600

    Lab 14 casi completado

--- Laboratorio 14/controllers/usuarios.controller.js
+++ Laboratorio 14/controllers/usuarios.controller.js
@@ -7,7 +7,7 @@ exports.get_login = (request, response, next) => {
 exports.post_login = (request, response, next) => {
     request.session.username = request.body.username;
     response.redirect('/');
-}
+};
 
 exports.get_logout = (request, response, next) => {
     request.session.destroy(() => {
